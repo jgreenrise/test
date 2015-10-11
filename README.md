28f713eb1f1828883d1b3ce692a5a4d9387ff6e6
jags4bitbucket <JatinPatel@Jatinkumars-MacBook-Pro.local>
2015-10-11 15:40:22 -0700

    running test via mvn

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 Download this for code coverage
 http://marketplace.eclipse.org/content/eclemma-java-code-coverage
 
-Help while running test
+Helps while running test
 https://infinitest.github.io
+
+# Recommended video course
+https://www.udemy.com/junitandmockitocrashcourse/#/lecture/3606662
--- junit_and_mockito/calculator/calculator/src/test/java/com/jsr/maven/calculator/CalculatorImplTest.java
+++ junit_and_mockito/calculator/calculator/src/test/java/com/jsr/maven/calculator/CalculatorImplTest.java
@@ -2,15 +2,35 @@ package com.jsr.maven.calculator;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class CalculatorImplTest {
+
+	private int num1, num2, result;
+
+	public CalculatorImplTest(int num1, int num2, int result) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.result = result;
+	}
+	
+	@Parameters
+	public static Collection<Integer[]> data(){
+		return Arrays.asList(new Integer [] [] {{-1, 2, 1}, {3,4,7}, {5,-10, -5}});
+	}
 
 	@Test
 	public void test_add() {
 		iCalculator cal = new CalculatorImpl();
-		int actual = cal.add(10,  20);
-		assertEquals(30, actual);
+		int actual = cal.add(num1, num2);
+		assertEquals(result, actual);
 	}
 
 }
